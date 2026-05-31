a9c31894c6d2f5177a1917c5cc9209df1c2390e9
Yashodh Fernando <99540008+Yashodh99@users.noreply.github.com>
2026-05-31 20:19:08 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 18.0"  # Forcing the older tutorial module version structure
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
